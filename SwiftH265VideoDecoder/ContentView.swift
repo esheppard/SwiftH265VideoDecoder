22d983aeba76044bb3f8fa24e15ebed5afc6206c
Elijah Sheppard <esheppard@gmail.com>
2024-08-11 09:47:30 +1000

    Initial Commit

---
+++ SwiftH265VideoDecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftH265VideoDecoder
+//
+//  Created by Elijah Sheppard on 11/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
